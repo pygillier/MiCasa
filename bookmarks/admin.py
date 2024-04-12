7a6e0c730b35dede351ab840b422f503b057a80b
Pierre-Yves Gillier <github@pygillier.me>
2024-04-12 22:13:03 +0200

    Bookmark admin

--- bookmarks/admin.py
+++ bookmarks/admin.py
@@ -4,9 +4,31 @@ from .models import BookmarkCategory, Bookmark
 
 @admin.register(BookmarkCategory)
 class BookmarkCategoryAdmin(admin.ModelAdmin):
-    list_display = ("name", "position")
+    list_display = ("name", "is_public", "position")
+
+    actions = ["make_public", "make_private"]
+
+    @admin.action(description="Set public")
+    def make_public(self, request, queryset):
+        queryset.update(is_public=True)
+
+    @admin.action(description="Set private")
+    def make_private(self, request, queryset):
+        queryset.update(is_public=False)
 
 
 @admin.register(Bookmark)
 class BookmarkAdmin(admin.ModelAdmin):
-    list_display = ("name", "category", "position")
+    list_display = ("name", "category", "is_public", "position")
+
+    list_filter = ["category", "is_public"]
+
+    actions = ["make_public", "make_private"]
+
+    @admin.action(description="Set public")
+    def make_public(self, request, queryset):
+        queryset.update(is_public=True)
+
+    @admin.action(description="Set private")
+    def make_private(self, request, queryset):
+        queryset.update(is_public=False)

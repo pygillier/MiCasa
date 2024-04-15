be9a8cb1d2b1defe60af07dd32289d69f3aa52ec
Pierre-Yves Gillier <github@pygillier.me>
2024-04-15 19:12:34 +0200

    Add basic logging and fix missing icon.

--- MiCasa/settings.py
+++ MiCasa/settings.py
@@ -93,6 +93,21 @@ DATABASES = {
     "default": env.db(),
 }
 
+# Logging config
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "WARNING",
+    },
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators

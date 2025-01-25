f6f61a197377983abaf1198a04efa46a8ed87f02
Pierre-Yves Gillier <github@pygillier.me>
2025-01-25 09:00:37 +0100

    Task runner for easier development

---
+++ tasks.py
@@ -0,0 +1,11 @@
+from invoke import task
+
+
+@task
+def build_css(c, component="manage"):
+    c.run(f"npx tailwindcss build -i static/src/{component}.css -o static/css/{component}.css --watch")
+
+
+@task(post=[build_css])
+def build_manage_css(c):
+    print("Building manage css")
